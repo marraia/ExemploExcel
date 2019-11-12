8f7ec701b215f7899c5e364254efea1245cca8f0
Fernando Mendes <fernando.mendes@xpi.com.br>
2019-11-12 08:59:54 -0300

    Novo

---
+++ ExemploExcel/Controllers/ExcelController.cs
@@ -0,0 +1,47 @@
+﻿using Bogus;
+using Bogus.Extensions.Brazil;
+using Microsoft.AspNetCore.Mvc;
+using OfficeOpenXml;
+
+namespace ExemploExcel.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ExcelController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult Get()
+        {
+            using (var excelPackage = new ExcelPackage())
+            {
+                //Cria um workspace
+                var worksheet = excelPackage.Workbook.Worksheets.Add("Novo Workspace");
+
+                //Cria o Header de cada coluna...
+                worksheet.Cells[1, 1].Style.Font.Bold = true;
+                worksheet.Cells[1, 1].Value = "Nome";
+                worksheet.Cells[1, 2].Style.Font.Bold = true;
+                worksheet.Cells[1, 2].Value = "CPF";
+                worksheet.Cells[1, 3].Style.Font.Bold = true;
+                worksheet.Cells[1, 3].Value = "Telefone";
+
+                //Cria cada linha em suas respectivasc colunas
+                for (var linha = 2; linha <= 101; linha++)
+                {
+                    var faker = new Faker("pt_BR");
+                    var cpf = faker.Person.Cpf().Replace(".", "").Replace("-", "").Trim();
+                    var name = faker.Person.FullName;
+                    var phone = faker.Person.Phone;
+
+                    worksheet.Cells[linha, 1].Value = name;
+                    worksheet.Cells[linha, 2].Value = cpf;
+                    worksheet.Cells[linha, 3].Value = phone;
+                }
+
+                return File(excelPackage.GetAsByteArray(), "application/vnd.ms-excel", "teste.xls");
+            }
+        }
+
+
+    }
+}
---
+++ ExemploExcel/Startup.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
+
+namespace ExemploExcel
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddSwaggerGen(s =>
+            {
+                s.SwaggerDoc("v1", new Info
+                {
+                    Version = "v1",
+                    Title = "Excel",
+                    Description = "Excel",
+                    Contact = new Contact { Name = "EX" },
+                });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+            app.UseSwagger();
+            app.UseSwaggerUI(s =>
+            {
+                s.SwaggerEndpoint("/swagger/v1/swagger.json", "Exemplo Excel");
+            });
+        }
+    }
+}
